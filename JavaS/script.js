dac3e685474e7fe1c8b45a1df1a86808500cb656
Bailin Amirkhan <reborn.0101kz@gmail.com>
2020-06-22 19:12:56 +0600

    first commit

---
+++ JavaS/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert('hey');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt("Are you 18+", "18");
+//console.log(answer+5);
+
+const answers = [];
+
+asnwers[0] = prompt('What is your name', '');
+asnwers[1] = prompt('What is your surname', '');
+asnwers[2] = prompt('What is your age', '');
+
+document.write(answers);
